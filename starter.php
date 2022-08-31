a8fe3e913afd319bebdf547a86a6c2f9aa50d1d4
Chamad Khadaev <chamad.khadaev@epitech.eu>
2022-08-31 11:11:54 +0200

    root change

--- starter.php
+++ starter.php
@@ -13,11 +13,11 @@
 <body>
     <header>
         <div class="button">
-            <a href=".vscode/intro.html"><button class="glow-on-hover" type="button"> READ! </button></a>
+            <a href="Read/.vscode/intro.html"><button class="glow-on-hover" type="button"> READ! </button></a>
         </div>
         <div class="media">
             <video autoplay loop muted id="bigbang">
-                <source src="VIDEO/bigbang.mp4" type="video/mp4">
+                <source src="Read/VIDEO/bigbang.mp4" type="video/mp4">
             </video>
         </div>
 
