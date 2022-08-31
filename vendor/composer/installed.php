b91cac8824d178ad01bbf116f7444f872f49fa63
Chamad Khadaev <chamad.khadaev@epitech.eu>
2022-08-31 11:03:00 +0200

    json fix + .lock add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/package_name',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2d96fc7f582ae689e757fdb2a620589a3b622ccb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/package_name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2d96fc7f582ae689e757fdb2a620589a3b622ccb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
